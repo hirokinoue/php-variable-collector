e8592ade2d34f7f7a44ff60ce4cb77a357f39656
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2022-02-26 16:02:36 +0900

    feat: PHPの変数を判定できるようにする

--- main.go
+++ main.go
@@ -7,3 +7,13 @@ import (
 func isPhpFile(s string) bool {
 	return strings.Contains(s, ".php")
 }
+
+func isPhpVariable(s string) bool {
+	if strings.Index(s, "$") != 0 {
+		return false
+	}
+	if strings.Contains(s, "->") {
+		return false
+	}
+	return true
+}
--- main_test.go
+++ main_test.go
@@ -36,3 +36,43 @@ func Test_isPhpFile(t *testing.T) {
 		})
 	}
 }
+
+func Test_isPhpVariable(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "PHPの変数であることが判定できる",
+			args: args{
+				s: "$hazekura",
+			},
+			want: true,
+		},
+		{
+			name: "$this->はPHPの変数とみなさない",
+			args: args{
+				s: "$this->hazekura",
+			},
+			want: false,
+		},
+		{
+			name: "PHPの変数でないことが判定できる",
+			args: args{
+				s: "hazekura",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPhpVariable(tt.args.s); got != tt.want {
+				t.Errorf("isPhpVariable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
